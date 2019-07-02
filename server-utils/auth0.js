6671226d34d1ed5aeceffb2a8fa8de9d737c4bd1
Sambego <sambellen@gmail.com>
2019-07-02 10:31:40 +0200

    Remove trello integration

--- server-utils/auth0.js
+++ server-utils/auth0.js
@@ -6,316 +6,277 @@ const helpers = require("./helpers");
 let creds;
 
 if (process.env.NODE_ENV === "prod") {
-  creds = process.env;
+    creds = process.env;
 } else {
-  creds = require("../credentials");
+    creds = require("../credentials");
 }
 
 function encodeData(data) {
-  for (let key in data) {
-    data[key] = encodeURIComponent(data[key]);
-  }
-  return data;
+    for (let key in data) {
+        data[key] = encodeURIComponent(data[key]);
+    }
+    return data;
 }
 
 function buildUrl(url, params) {
-  let queryParams = "";
-  params = encodeData(params);
-  for (let key in params) {
-    queryParams += `${key}=${params[key]}&`;
-  }
-  console.log(`Preparing ${url}?${queryParams}`);
-  return `${url}?${queryParams}`;
+    let queryParams = "";
+    params = encodeData(params);
+    for (let key in params) {
+        queryParams += `${key}=${params[key]}&`;
+    }
+    console.log(`Preparing ${url}?${queryParams}`);
+    return `${url}?${queryParams}`;
 }
 
 function getLocation(event) {
-  if (event.location) return event.location;
-  if (event.state) return `${event.city}, ${event.state}, ${event.country}`;
-  return `${event.city}, ${event.country}`;
+    if (event.location) return event.location;
+    if (event.state) return `${event.city}, ${event.state}, ${event.country}`;
+    return `${event.city}, ${event.country}`;
 }
 
 function getToFromDates(event) {
-  const startDate = helpers.dateFormat(event.startDate);
-  if (!event.endDate) return startDate;
-  const endDate = helpers.dateFormat(event.endDate);
-  if (event.startDate === event.endDate) return startDate;
-  return `${startDate} to ${endDate}`;
+    const startDate = helpers.dateFormat(event.startDate);
+    if (!event.endDate) return startDate;
+    const endDate = helpers.dateFormat(event.endDate);
+    if (event.startDate === event.endDate) return startDate;
+    return `${startDate} to ${endDate}`;
 }
 
 function getTalkTitles(talk) {
-  if (talk.title) return talk.title;
-  return talk.map(t => t.title).join(", ");
+    if (talk.title) return talk.title;
+    return talk.map(t => t.title).join(", ");
 }
 
 function getWebsite(event) {
-  if (event.url) return event.url;
-  return `https://meetup.com/${event.meetupUrlName}`;
+    if (event.url) return event.url;
+    return `https://meetup.com/${event.meetupUrlName}`;
 }
 
 function addConferenceToSheet(c, t, s, isMeetup) {
-  const url = "https://hooks.zapier.com/hooks/catch/5126522/v7ov4y/";
-  const data = {
-    conference: c.name,
-    start: helpers.convertTimestampToMMDYY(c.startDate),
-    end: helpers.convertTimestampToMMDYY(c.endDate ? c.endDate : c.startDate),
-    twitter: isMeetup ? "" : c.twitter,
-    website: getWebsite(c),
-    location: getLocation(c),
-    talk0: t[0].title,
-    talk1: t[1] ? t[1].title : "",
-    speaker: s.name
-  };
-
-  let hook = buildUrl(url, data);
-  return axios.get(hook).catch(err => {
-    console.log("Error adding to sheet", err);
-  });
-}
-
-function createSLK(c, t, s) {
-  let _browser;
-  let _page;
-  const filename = `${__dirname}/tmp/${new Date().getTime()}.png`;
-
-  puppeteer
-    .launch()
-    .then(browser => {
-      return (_browser = browser);
-    })
-    .then(browser => {
-      return (_page = browser.newPage());
-    })
-    .then(page => {
-      return page.goto(c.url);
-    })
-    .then(() => {
-      return _page;
-    })
-    .then(page => {
-      page.setViewport({ width: 411, height: 823 });
-    })
-    .then(() => {
-      return _page;
-    })
-    .then(page => {
-      return page.screenshot({ path: filename });
-    })
-    .then(() => {
-      return _browser.close();
-    })
-    .then(_ => {
-      console.log("We got a screenshot");
-      //Upload to cloudinary
-      cloudinary.config({
-        cloud_name: creds.CLOUDINARY_CLOUD_NAME,
-        api_key: creds.CLOUDINARY_API_KEY,
-        api_secret: creds.CLOUDINARY_API_SECRET
-      });
-
-      return new Promise((resolve, reject) => {
-        cloudinary.v2.uploader.upload(filename, function(err, result) {
-          if (err) return reject(err);
-          resolve(result);
-        });
-      });
-    })
-    .then(result => {
-      console.log("Sent to Cloudinary");
-      const cloudinaryURL = result.secure_url;
-
-      const url = "https://hooks.zapier.com/hooks/catch/5126522/v7y6iy/";
-
-      const data = {
-        name: c.name,
-        overview: c.overview,
-        dates: getToFromDates(c),
-        twitter: c.twitter,
-        attendee_goal: c.attendeeGoal,
-        relationships_goal: c.relationshipGoal,
-        speaker: s.name,
-        talks: t.map(talk => talk.title).join("\n"),
-        screenshot: `${cloudinaryURL}`,
-        website: c.url,
+    const url = "https://hooks.zapier.com/hooks/catch/5126522/v7ov4y/";
+    const data = {
+        conference: c.name,
+        start: helpers.convertTimestampToMMDYY(c.startDate),
+        end: helpers.convertTimestampToMMDYY(c.endDate ? c.endDate : c.startDate),
+        twitter: isMeetup ? "" : c.twitter,
+        website: getWebsite(c),
         location: getLocation(c),
-        conference_id: c.id
-      };
+        talk0: t[0].title,
+        talk1: t[1] ? t[1].title : "",
+        speaker: s.name
+    };
 
-      let hook = buildUrl(url, data);
-      return axios.get(hook).catch(err => {
-        console.log("Error creating SLK", err);
-      });
+    let hook = buildUrl(url, data);
+    return axios.get(hook).catch(err => {
+        console.log("Error adding to sheet", err);
     });
 }
 
+function createSLK(c, t, s) {
+    let _browser;
+    let _page;
+    const filename = `${__dirname}/tmp/${new Date().getTime()}.png`;
+
+    puppeteer
+        .launch()
+        .then(browser => {
+            return (_browser = browser);
+        })
+        .then(browser => {
+            return (_page = browser.newPage());
+        })
+        .then(page => {
+            return page.goto(c.url);
+        })
+        .then(() => {
+            return _page;
+        })
+        .then(page => {
+            page.setViewport({ width: 411, height: 823 });
+        })
+        .then(() => {
+            return _page;
+        })
+        .then(page => {
+            return page.screenshot({ path: filename });
+        })
+        .then(() => {
+            return _browser.close();
+        })
+        .then(_ => {
+            console.log("We got a screenshot");
+            //Upload to cloudinary
+            cloudinary.config({
+                cloud_name: creds.CLOUDINARY_CLOUD_NAME,
+                api_key: creds.CLOUDINARY_API_KEY,
+                api_secret: creds.CLOUDINARY_API_SECRET
+            });
+
+            return new Promise((resolve, reject) => {
+                cloudinary.v2.uploader.upload(filename, function(err, result) {
+                    if (err) return reject(err);
+                    resolve(result);
+                });
+            });
+        })
+        .then(result => {
+            console.log("Sent to Cloudinary");
+            const cloudinaryURL = result.secure_url;
+
+            const url = "https://hooks.zapier.com/hooks/catch/5126522/v7y6iy/";
+
+            const data = {
+                name: c.name,
+                overview: c.overview,
+                dates: getToFromDates(c),
+                twitter: c.twitter,
+                attendee_goal: c.attendeeGoal,
+                relationships_goal: c.relationshipGoal,
+                speaker: s.name,
+                talks: t.map(talk => talk.title).join("\n"),
+                screenshot: `${cloudinaryURL}`,
+                website: c.url,
+                location: getLocation(c),
+                conference_id: c.id
+            };
+
+            let hook = buildUrl(url, data);
+            return axios.get(hook).catch(err => {
+                console.log("Error creating SLK", err);
+            });
+        });
+}
+
 function sendSlackMessage(c, t, s, isMeetup) {
-  const url = "https://hooks.zapier.com/hooks/catch/5126522/v7o9we/";
+    const url = "https://hooks.zapier.com/hooks/catch/5126522/v7o9we/";
 
-  const data = {
-    conference: isMeetup ? `Meetup ${c.name}` : c.name,
-    speaker: s.name,
-    location: getLocation(c),
-    dates: getToFromDates(c)
-  };
+    const data = {
+        conference: isMeetup ? `Meetup ${c.name}` : c.name,
+        speaker: s.name,
+        location: getLocation(c),
+        dates: getToFromDates(c)
+    };
 
-  let hook = buildUrl(url, data);
-  return axios.get(hook).catch(err => {
-    console.log("Error sending Slack message", err);
-  });
+    let hook = buildUrl(url, data);
+    return axios.get(hook).catch(err => {
+        console.log("Error sending Slack message", err);
+    });
 }
 
 function addToEvangelistCalendar(c, t, s) {
-  const url = "https://hooks.zapier.com/hooks/catch/5126522/vnp60u/";
+    const url = "https://hooks.zapier.com/hooks/catch/5126522/vnp60u/";
 
-  const data = {
-    conference: c.name,
-    speaker: s.name,
-    talks: getTalkTitles(t),
-    start: helpers.convertTimestampToMMDYY(c.startDate),
-    end: helpers.convertTimestampToMMDYY(c.endDate ? c.endDate : c.startDate),
-    overview: c.overview,
-    location: getLocation(c)
-  };
+    const data = {
+        conference: c.name,
+        speaker: s.name,
+        talks: getTalkTitles(t),
+        start: helpers.convertTimestampToMMDYY(c.startDate),
+        end: helpers.convertTimestampToMMDYY(c.endDate ? c.endDate : c.startDate),
+        overview: c.overview,
+        location: getLocation(c)
+    };
 
-  let hook = buildUrl(url, data);
-  return axios.get(hook).catch(err => {
-    console.log("Error adding to evangelism calendar", err);
-  });
-}
-
-function addToTrello(c, t, s) {
-  const url = "https://hooks.zapier.com/hooks/catch/5126522/vnvpe0/";
-
-  const data = {
-    conference: c.name,
-    speaker: s.name,
-    talks: t.map(talk => talk.title).join(", "),
-    start: helpers.convertTimestampToMMDYY(c.startDate),
-    overview: c.overview,
-    location: getLocation(c),
-    website: c.url
-  };
-
-  let hook = buildUrl(url, data);
-  return axios.get(hook).catch(err => {
-    console.log("Error adding to Trello", err);
-  });
+    let hook = buildUrl(url, data);
+    return axios.get(hook).catch(err => {
+        console.log("Error adding to evangelism calendar", err);
+    });
 }
 
 function addToCommunityForums(c, t, s) {
-  const url = "https://hooks.zapier.com/hooks/catch/5126522/v7yw5a/";
+    const url = "https://hooks.zapier.com/hooks/catch/5126522/v7yw5a/";
 
-  const data = {
-    communityUsername: s.communityUsername,
-    talks: getTalkTitles(t),
-    conference: c.name,
-    location: getLocation(c),
-    website: getWebsite(c),
-    dates: getToFromDates(c)
-  };
+    const data = {
+        communityUsername: s.communityUsername,
+        talks: getTalkTitles(t),
+        conference: c.name,
+        location: getLocation(c),
+        website: getWebsite(c),
+        dates: getToFromDates(c)
+    };
 
-  let hook = buildUrl(url, data);
-  return axios.get(hook).catch(err => {
-    console.log("Error adding to Community Forums", err);
-  });
+    let hook = buildUrl(url, data);
+    return axios.get(hook).catch(err => {
+        console.log("Error adding to Community Forums", err);
+    });
 }
 
 function addToMarketingRoadmap(c, t, s, isMeetup) {
-  const url = "https://hooks.zapier.com/hooks/catch/5126522/v7ypro/";
+    const url = "https://hooks.zapier.com/hooks/catch/5126522/v7ypro/";
 
-  const data = {
-    start: helpers.convertTimestampToMMDYY(c.startDate),
-    conference: c.name,
-    region: c.regionRoadmapValue,
-    website: getWebsite(c),
-    speaker: s.name,
-    owner: s.email,
-    overview: isMeetup ? "" : c.overview,
-    topic: isMeetup ? "Meetup" : "Conference",
-    location: getLocation(c)
-  };
+    const data = {
+        start: helpers.convertTimestampToMMDYY(c.startDate),
+        conference: c.name,
+        region: c.regionRoadmapValue,
+        website: getWebsite(c),
+        speaker: s.name,
+        owner: s.email,
+        overview: isMeetup ? "" : c.overview,
+        topic: isMeetup ? "Meetup" : "Conference",
+        location: getLocation(c)
+    };
 
-  let hook = buildUrl(url, data);
-  return axios.get(hook).catch(err => {
-    console.log("Error adding to Marketing Roadmap", err);
-  });
-}
-
-function addMeetupToTrello(m, t, s) {
-  const url = "https://hooks.zapier.com/hooks/catch/5126522/v7ybv5/";
-
-  const data = {
-    conference: `Meetup ${m.name}`,
-    location: getLocation(m),
-    speaker: s.name,
-    talks: t.title,
-    website: `https://meetup.com/${m.meetupUrlName}`,
-    start: helpers.convertTimestampToMMDYY(m.startDate)
-  };
-
-  let hook = buildUrl(url, data);
-  return axios.get(hook).catch(err => {
-    console.log("Error adding Meetup to Trello", err);
-  });
+    let hook = buildUrl(url, data);
+    return axios.get(hook).catch(err => {
+        console.log("Error adding to Marketing Roadmap", err);
+    });
 }
 
 function addToDevelopersReachedSheet(r) {
-  let url = "https://hooks.zapier.com/hooks/catch/5126522/v7ouhu/";
+    let url = "https://hooks.zapier.com/hooks/catch/5126522/v7ouhu/";
 
-  const data = {
-    eventName: r.eventName,
-    type: r.type,
-    source: r.source,
-    formattedDate: helpers.convertTimestampToMMDYY(r.eventDate),
-    developersReached: r.developersReached,
-    region: r.region,
-    quarter: helpers.getQuarter(r.eventDate)
-  };
+    const data = {
+        eventName: r.eventName,
+        type: r.type,
+        source: r.source,
+        formattedDate: helpers.convertTimestampToMMDYY(r.eventDate),
+        developersReached: r.developersReached,
+        region: r.region,
+        quarter: helpers.getQuarter(r.eventDate)
+    };
 
-  let hook = buildUrl(url, data);
-  return axios.get(hook).catch(err => {
-    console.log("Error adding to developers reached sheet");
-  });
+    let hook = buildUrl(url, data);
+    return axios.get(hook).catch(err => {
+        console.log("Error adding to developers reached sheet");
+    });
 }
 
 function conferenceApproved(conference, talks, speaker) {
-  const promiseArray = [
-    addConferenceToSheet(conference, talks, speaker),
-    createSLK(conference, talks, speaker),
-    sendSlackMessage(conference, talks, speaker),
-    addToTrello(conference, talks, speaker),
-    addToCommunityForums(conference, talks, speaker),
-    addToMarketingRoadmap(conference, talks, speaker)
-  ];
+    const promiseArray = [
+        addConferenceToSheet(conference, talks, speaker),
+        createSLK(conference, talks, speaker),
+        sendSlackMessage(conference, talks, speaker),
+        addToCommunityForums(conference, talks, speaker),
+        addToMarketingRoadmap(conference, talks, speaker)
+    ];
 
-  return Promise.all(promiseArray).then(_ => {
-    console.log("All hooks completed for conference acceptance");
-  });
+    return Promise.all(promiseArray).then(_ => {
+        console.log("All hooks completed for conference acceptance");
+    });
 }
 
 function meetupApproved(meetup, talk, speaker) {
-  const promiseArray = [
-    sendSlackMessage(meetup, talk, speaker, true),
-    addConferenceToSheet(meetup, [talk], speaker, true),
-    addMeetupToTrello(meetup, talk, speaker),
-    addToCommunityForums(meetup, talk, speaker),
-    addToMarketingRoadmap(meetup, talk, speaker, true)
-  ];
+    const promiseArray = [
+        sendSlackMessage(meetup, talk, speaker, true),
+        addConferenceToSheet(meetup, [talk], speaker, true),
+        addToCommunityForums(meetup, talk, speaker),
+        addToMarketingRoadmap(meetup, talk, speaker, true)
+    ];
 
-  return Promise.all(promiseArray).then(_ => {
-    console.log("All hooks completed for meetup acceptance");
-  });
+    return Promise.all(promiseArray).then(_ => {
+        console.log("All hooks completed for meetup acceptance");
+    });
 }
 
 function postConferenceReport(report) {
-  const promiseArray = [addToDevelopersReachedSheet(report)];
+    const promiseArray = [addToDevelopersReachedSheet(report)];
 
-  return Promise.all(promiseArray).then(_ => {
-    console.log("All hooks completed for post conference report");
-  });
+    return Promise.all(promiseArray).then(_ => {
+        console.log("All hooks completed for post conference report");
+    });
 }
 
 module.exports = {
-  conferenceApproved,
-  meetupApproved,
-  postConferenceReport
+    conferenceApproved,
+    meetupApproved,
+    postConferenceReport
 };
--- server-utils/zapier.js
+++ server-utils/zapier.js
@@ -5,172 +5,155 @@ const cloudinary = require("cloudinary");
 let creds;
 
 if (process.env.NODE_ENV === "prod") {
-  creds = process.env;
+    creds = process.env;
 } else {
-  creds = require("../credentials");
+    creds = require("../credentials");
 }
 
 const URL = {
-  addConferenceToSheet: "https://hooks.zapier.com/hooks/catch/5126522/v7ov4y/",
-  createSLK: "https://hooks.zapier.com/hooks/catch/5126522/v7y6iy/",
-  sendSlackMessage: "https://hooks.zapier.com/hooks/catch/5126522/v7o9we/",
-  addToEvangelistCalendar:
-    "https://hooks.zapier.com/hooks/catch/5126522/vnp60u/",
-  addToTrello: "https://hooks.zapier.com/hooks/catch/5126522/vnvpe0/",
-  addMeetupToTrello: "https://hooks.zapier.com/hooks/catch/5126522/v7ybv5/",
-  addToCommunityForums: "https://hooks.zapier.com/hooks/catch/5126522/v7yw5a/",
-  addToMarketingRoadmap: "https://hooks.zapier.com/hooks/catch/5126522/v7ypro/",
-  addToDevelopersReached: "https://hooks.zapier.com/hooks/catch/5126522/v7ouhu/"
+    addConferenceToSheet: "https://hooks.zapier.com/hooks/catch/5126522/v7ov4y/",
+    createSLK: "https://hooks.zapier.com/hooks/catch/5126522/v7y6iy/",
+    sendSlackMessage: "https://hooks.zapier.com/hooks/catch/5126522/v7o9we/",
+    addToEvangelistCalendar: "https://hooks.zapier.com/hooks/catch/5126522/vnp60u/",
+    addToCommunityForums: "https://hooks.zapier.com/hooks/catch/5126522/v7yw5a/",
+    addToMarketingRoadmap: "https://hooks.zapier.com/hooks/catch/5126522/v7ypro/",
+    addToDevelopersReached: "https://hooks.zapier.com/hooks/catch/5126522/v7ouhu/"
 };
 
 function buildUrl(url, params) {
-  let queryParams = "";
-  for (let key in params) {
-    queryParams += `${key}=${params[key]}&`;
-  }
-  return `${url}?${queryParams}`;
+    let queryParams = "";
+    for (let key in params) {
+        queryParams += `${key}=${params[key]}&`;
+    }
+    return `${url}?${queryParams}`;
 }
 
 function encodeData(data) {
-  for (let key in data) {
-    data[key] = encodeURIComponent(data[key]);
-  }
-  return data;
+    for (let key in data) {
+        data[key] = encodeURIComponent(data[key]);
+    }
+    return data;
 }
 
 Zapier = {
-  meetupApproved: data => {
-    console.log("Zapierizing, meetup confirmed");
+    meetupApproved: data => {
+        console.log("Zapierizing, meetup confirmed");
 
-    data.topic = "Meetup";
-    data.conference = "Meetup " + data.conference;
-    data = encodeData(data);
-    let promiseArray = [
-      Zapier.sendSlackMessage(data),
-      Zapier.addConferenceToSheet(data),
-      Zapier.addMeetupToTrello(data),
-      Zapier.addToCommunityForums(data),
-      Zapier.addToMarketingRoadmap(data)
-    ];
-    Promise.all(promiseArray).then(data => {
-      console.log("Done with Zapier");
-    });
-  },
-  approved: data => {
-    console.log("Zapierizing, accepted at conference");
-
-    data.topic = "Conference";
-    data = encodeData(data);
-    let promiseArray = [
-      Zapier.addConferenceToSheet(data),
-      Zapier.createSLK(data),
-      Zapier.sendSlackMessage(data),
-      Zapier.addToTrello(data),
-      Zapier.addToCommunityForums(data),
-      Zapier.addToMarketingRoadmap(data)
-    ];
-
-    Promise.all(promiseArray).then(data => {
-      console.log("Done with Zapier");
-    });
-  },
-  report: data => {
-    console.log("Zapierizing, new report");
-    let promiseArray = [Zapier.addToDevelopersReached(data)];
-
-    Promise.all(promiseArray).then(data => {
-      console.log("Done with Zapier");
-    });
-  },
-  addConferenceToSheet: data => {
-    let url = buildUrl(URL.addConferenceToSheet, data);
-    return axios.get(url).catch(err => {
-      console.log("Error adding to sheet", err);
-    });
-  },
-  createSLK: data => {
-    // Get Screenshot
-    let pageres = new Pageres({ delay: 0 })
-      .src(data.website, ["iphone 5s"], { crop: true })
-      .dest(__dirname + "/tmp")
-      .run();
-    return pageres
-      .then(data => {
-        console.log("We got a screenshot");
-        //Upload to cloudinary
-        cloudinary.config({
-          cloud_name: creds.CLOUDINARY_CLOUD_NAME,
-          api_key: creds.CLOUDINARY_API_KEY,
-          api_secret: creds.CLOUDINARY_API_SECRET
+        data.topic = "Meetup";
+        data.conference = "Meetup " + data.conference;
+        data = encodeData(data);
+        let promiseArray = [
+            Zapier.sendSlackMessage(data),
+            Zapier.addConferenceToSheet(data),
+            Zapier.addToCommunityForums(data),
+            Zapier.addToMarketingRoadmap(data)
+        ];
+        Promise.all(promiseArray).then(data => {
+            console.log("Done with Zapier");
         });
+    },
+    approved: data => {
+        console.log("Zapierizing, accepted at conference");
 
-        return new Promise((resolve, reject) => {
-          cloudinary.v2.uploader.upload(
-            __dirname + "/tmp/" + data[0].filename,
-            function(err, result) {
-              if (err) return reject(err);
-              resolve(result);
-            }
-          );
+        data.topic = "Conference";
+        data = encodeData(data);
+        let promiseArray = [
+            Zapier.addConferenceToSheet(data),
+            Zapier.createSLK(data),
+            Zapier.sendSlackMessage(data),
+            Zapier.addToCommunityForums(data),
+            Zapier.addToMarketingRoadmap(data)
+        ];
+
+        Promise.all(promiseArray).then(data => {
+            console.log("Done with Zapier");
         });
-      })
-      .then(result => {
-        console.log("Sent to Cloudinary");
-        // Add image URL
-        data.image = result.secure_url;
-      })
-      .then(() => {
-        let url = buildUrl(URL.createSLK, data);
-        console.log("Sending to Zapier", url);
-        return axios.get(url);
-      })
-      .catch(err => {
-        console.log("Error creating SLK", err);
-      });
-  },
-  sendSlackMessage: data => {
-    let url = buildUrl(URL.sendSlackMessage, data);
-    return axios.get(url).catch(err => {
-      console.log("Error sending message on Slack", err);
-    });
-  },
-  addToEvangelistCalendar: data => {
-    let url = buildUrl(URL.addToEvangelistCalendar, data);
-    return axios.get(url).catch(err => {
-      console.log("Error adding to calendar", err);
-    });
-  },
-  addToTrello: data => {
-    let url = buildUrl(URL.addToTrello, data);
-    return axios.get(url).catch(err => {
-      console.log("Error adding to Trello", err);
-    });
-  },
-  addMeetupToTrello: data => {
-    let url = buildUrl(URL.addMeetupToTrello, data);
-    return axios.get(url).catch(err => {
-      console.log("Error adding meetup to Trello", err);
-    });
-  },
-  addToCommunityForums: data => {
-    let url = buildUrl(URL.addToCommunityForums, data);
-    return axios.get(url).catch(err => {
-      console.log("Error adding to Discourse", err);
-    });
-  },
-  addToMarketingRoadmap: data => {
-    data.topic = "Conference";
-    let url = buildUrl(URL.addToMarketingRoadmap, data);
-    return axios.get(url).catch(err => {
-      console.log("Error adding to marketing roadmap", err);
-    });
-  },
-  addToDevelopersReached: data => {
-    let url = buildUrl(URL.addToDevelopersReached, data);
-    return axios.get(url).catch(err => {
-      console.log("Error adding to Developers Reached sheet", err);
-    });
-  }
+    },
+    report: data => {
+        console.log("Zapierizing, new report");
+        let promiseArray = [Zapier.addToDevelopersReached(data)];
+
+        Promise.all(promiseArray).then(data => {
+            console.log("Done with Zapier");
+        });
+    },
+    addConferenceToSheet: data => {
+        let url = buildUrl(URL.addConferenceToSheet, data);
+        return axios.get(url).catch(err => {
+            console.log("Error adding to sheet", err);
+        });
+    },
+    createSLK: data => {
+        // Get Screenshot
+        let pageres = new Pageres({ delay: 0 })
+            .src(data.website, ["iphone 5s"], { crop: true })
+            .dest(__dirname + "/tmp")
+            .run();
+        return pageres
+            .then(data => {
+                console.log("We got a screenshot");
+                //Upload to cloudinary
+                cloudinary.config({
+                    cloud_name: creds.CLOUDINARY_CLOUD_NAME,
+                    api_key: creds.CLOUDINARY_API_KEY,
+                    api_secret: creds.CLOUDINARY_API_SECRET
+                });
+
+                return new Promise((resolve, reject) => {
+                    cloudinary.v2.uploader.upload(
+                        __dirname + "/tmp/" + data[0].filename,
+                        function(err, result) {
+                            if (err) return reject(err);
+                            resolve(result);
+                        }
+                    );
+                });
+            })
+            .then(result => {
+                console.log("Sent to Cloudinary");
+                // Add image URL
+                data.image = result.secure_url;
+            })
+            .then(() => {
+                let url = buildUrl(URL.createSLK, data);
+                console.log("Sending to Zapier", url);
+                return axios.get(url);
+            })
+            .catch(err => {
+                console.log("Error creating SLK", err);
+            });
+    },
+    sendSlackMessage: data => {
+        let url = buildUrl(URL.sendSlackMessage, data);
+        return axios.get(url).catch(err => {
+            console.log("Error sending message on Slack", err);
+        });
+    },
+    addToEvangelistCalendar: data => {
+        let url = buildUrl(URL.addToEvangelistCalendar, data);
+        return axios.get(url).catch(err => {
+            console.log("Error adding to calendar", err);
+        });
+    },
+    addToCommunityForums: data => {
+        let url = buildUrl(URL.addToCommunityForums, data);
+        return axios.get(url).catch(err => {
+            console.log("Error adding to Discourse", err);
+        });
+    },
+    addToMarketingRoadmap: data => {
+        data.topic = "Conference";
+        let url = buildUrl(URL.addToMarketingRoadmap, data);
+        return axios.get(url).catch(err => {
+            console.log("Error adding to marketing roadmap", err);
+        });
+    },
+    addToDevelopersReached: data => {
+        let url = buildUrl(URL.addToDevelopersReached, data);
+        return axios.get(url).catch(err => {
+            console.log("Error adding to Developers Reached sheet", err);
+        });
+    }
 };
 
 module.exports = Zapier;
